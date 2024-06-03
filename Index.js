da29620668ed656541b55a2da1729aa95221a01e
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-06-04 00:29:37 +0100

    added tabbed components and more styles

--- Index.js
+++ Index.js
@@ -1,1 +1,17 @@
 'use strict';
+let imgbox = document.getElementById('imagebox');
+let img = document.getElementById('img');
+const imagesArray = ["images/DSC_0290 (1) (1).jpg", "images/IMG_20220929_084524.jpg"];
+
+let numImages = imagesArray.length;
+let i = 0; 
+
+setInterval( function() {
+  let src = imagesArray[i];
+  img.src = src;
+  i++;
+  if (i == numImages) {
+    i = 0;
+  }
+
+}, 6000); // Change every 6 seconds

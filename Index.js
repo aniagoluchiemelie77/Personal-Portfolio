319e2a521d8fef49c54a19260f0a7f231624818e
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-06-11 11:14:01 +0100

    built tabbed component

--- Index.js
+++ Index.js
@@ -2,6 +2,8 @@
 let imgbox = document.getElementById('imagebox');
 let img = document.getElementById('img');
 const imagesArray = ["images/DSC_0290 (1) (1).jpg", "images/IMG_20220929_084524.jpg"];
+const tabs = document.querySelectorAll('.tablinks');
+const tabContent = document.querySelectorAll('.tabcontent');
 
 let numImages = imagesArray.length;
 let i = 0; 
@@ -15,3 +17,20 @@ setInterval( function() {
   }
 
 }, 6000); // Change every 6 seconds
+
+//Script for tabs
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    tab.classList.add('active');
+    const line = document.querySelector('.line');
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
+    tabContent.forEach((content) =>{
+      content.classList.remove('active');
+    })
+    tabContent[index].classList.add('active');
+  })
+})

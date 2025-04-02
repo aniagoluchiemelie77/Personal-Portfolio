fcdbf9b848e5f811ca3d7bcfccf06cecb78098eb
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2025-04-02 17:19:43 +0100

    Added animations.

--- Index.js
+++ Index.js
@@ -7,30 +7,4 @@ const tabContent = document.querySelectorAll('.tabcontent');
 
 let numImages = imagesArray.length;
 let i = 0; 
-
-setInterval( function() {
-  let src = imagesArray[i];
-  img.src = src;
-  i++;
-  if (i == numImages) {
-    i = 0;
-  }
-
-}, 6000); // Change every 6 seconds
-
-//Script for tabs
-tabs.forEach((tab, index) => {
-  tab.addEventListener('click', (e) => {
-    tabs.forEach((tab) => {
-      tab.classList.remove('active');
-    });
-    tab.classList.add('active');
-    const line = document.querySelector('.line');
-    line.style.width = e.target.offsetWidth + "px";
-    line.style.left = e.target.offsetLeft + "px";
-    tabContent.forEach((content) =>{
-      content.classList.remove('active');
-    })
-    tabContent[index].classList.add('active');
-  })
-})
+AOS.init();
